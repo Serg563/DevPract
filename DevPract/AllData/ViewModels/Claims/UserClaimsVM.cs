a44b63d7088f2eaf85ef7dafebe4bc2caa9f8c55
Fanger <Niverchuksergii333@gmail.com>
2023-01-29 12:18:24 +0200

    Added claim's classes

---
+++ DevPract/AllData/ViewModels/Claims/UserClaimsVM.cs
@@ -0,0 +1,15 @@
+﻿using Duende.IdentityServer.EntityFramework.Entities;
+
+namespace DevPract.AllData.ViewModels.Claims
+{
+    public class UserClaimsVM
+    {
+        public UserClaimsVM()
+        {
+            Cliams = new List<UserClaim>();
+        }
+
+        public string UserId { get; set; }
+        public List<UserClaim> Cliams { get; set; }
+    }
+}
